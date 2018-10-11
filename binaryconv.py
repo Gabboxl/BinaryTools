3e1335948e6072ff7ded825fcec9e9efdd28d48f
Gabriele <gabboxl0@gmail.com>
2018-10-11 20:13:56 +0200

    added binaryconv.py
    binary conversion to decimal

---
+++ binaryconv.py
@@ -0,0 +1,13 @@
+while True:
+    num = input("Inserisci un numero binario da convertire in decimale: ")
+    binario = list(map(int, num))
+    binario.reverse()
+    snumeri = []
+    asd = 0
+    for numero in binario:
+        # if numero == 0 or numero == 1:
+        snumeri.append(pow(2, asd) * numero)
+        asd += 1
+
+    print(sum(snumeri))
+    print(snumeri)
